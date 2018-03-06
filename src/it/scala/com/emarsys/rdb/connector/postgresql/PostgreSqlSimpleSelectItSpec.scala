7b457b3e7cf75890d7b2158f1da43374da031bdc
khorvath <kristof.horvath@emarsys.com>
2018-03-06 14:20:06 +0100

    [simple_select] add optional distinct flag

--- src/it/scala/com/emarsys/rdb/connector/postgresql/PostgreSqlSimpleSelectItSpec.scala
+++ src/it/scala/com/emarsys/rdb/connector/postgresql/PostgreSqlSimpleSelectItSpec.scala
@@ -3,12 +3,14 @@ package com.emarsys.rdb.connector.postgresql
 import akka.actor.ActorSystem
 import akka.stream.{ActorMaterializer, Materializer}
 import akka.testkit.TestKit
-import com.emarsys.rdb.connector.postgresql.utils.SelectDbInitHelper
+import com.emarsys.rdb.connector.postgresql.utils.{SelectDbInitHelper, TestHelper}
 import com.emarsys.rdb.connector.test.SimpleSelectItSpec
 
 import concurrent.duration._
+import scala.concurrent.Await
 
 class PostgreSqlSimpleSelectItSpec extends TestKit(ActorSystem()) with SimpleSelectItSpec with SelectDbInitHelper {
+  import scala.concurrent.ExecutionContext.Implicits.global
 
   override implicit val materializer: Materializer = ActorMaterializer()
 
@@ -17,6 +19,33 @@ class PostgreSqlSimpleSelectItSpec extends TestKit(ActorSystem()) with SimpleSel
   override def afterAll(): Unit = {
     system.terminate()
     super.afterAll()
+  }
+
+  override def initDb(): Unit = {
+    super.initDb()
+
+    val createCTableSql =
+      s"""CREATE TABLE "$cTableName" (
+         |    C varchar(255) NOT NULL
+         |);""".stripMargin
+
+    val insertCDataSql =
+      s"""INSERT INTO "$cTableName" (C) VALUES
+         |('c12'),
+         |('c12'),
+         |('c3')
+         |;""".stripMargin
+
+    Await.result(for {
+      _ <- TestHelper.executeQuery(createCTableSql)
+      _ <- TestHelper.executeQuery(insertCDataSql)
+    } yield (), 5.seconds)
+  }
+
+  override def cleanUpDb(): Unit = {
+    val dropCTableSql = s"""DROP TABLE "$cTableName";"""
+    Await.result(TestHelper.executeQuery(dropCTableSql), 5.seconds)
+    super.cleanUpDb()
   }
 
 }
