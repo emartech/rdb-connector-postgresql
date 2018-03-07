46716da29c728177ff4d39ae389b672f39289d94
khorvath <kristof.horvath@emarsys.com>
2018-03-07 14:41:39 +0100

    [search] implemented

--- build.sbt
+++ build.sbt
@@ -19,12 +19,12 @@ libraryDependencies ++= {
   val scalaTestV = "3.0.1"
   val slickV = "3.2.0"
   Seq(
-    "com.github.emartech" %  "rdb-connector-common" % "76c48422d9",
+    "com.github.emartech" %  "rdb-connector-common" % "67bc7a5e27",
     "com.typesafe.slick"  %% "slick"                % slickV,
     "com.typesafe.slick"  %% "slick-hikaricp"       % slickV,
     "org.scalatest"       %% "scalatest"            % scalaTestV   % Test,
     "com.typesafe.akka"   %% "akka-stream-testkit"  % "2.5.6"      % Test,
-    "com.github.emartech" %  "rdb-connector-test"   % "09f1e2e0f6" % Test,
+    "com.github.emartech" %  "rdb-connector-test"   % "299379b749" % Test,
     "org.postgresql"      %  "postgresql"           % "42.1.4",
     "com.typesafe.akka"   %% "akka-http-spray-json" % "10.0.7"     % Test,
     "org.mockito"         %  "mockito-core"         % "2.11.0"     % Test
---
+++ src/it/scala/com/emarsys/rdb/connector/postgresql/PostgreSqlSearchItSpec.scala
@@ -0,0 +1,67 @@
+package com.emarsys.rdb.connector.postgresql
+
+import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, Materializer}
+import akka.testkit.TestKit
+import com.emarsys.rdb.connector.common.models.Connector
+import com.emarsys.rdb.connector.postgresql.utils.TestHelper
+import com.emarsys.rdb.connector.test.SearchItSpec
+import slick.util.AsyncExecutor
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class PostgreSqlSearchItSpec extends TestKit(ActorSystem()) with SearchItSpec {
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  val connector: Connector = Await.result(PostgreSqlConnector(TestHelper.TEST_CONNECTION_CONFIG)(AsyncExecutor.default()), 5.seconds).right.get
+
+  override implicit val materializer: Materializer = ActorMaterializer()
+
+  override val awaitTimeout = 15.seconds
+
+  override def afterAll(): Unit = {
+    system.terminate()
+    super.afterAll()
+  }
+
+  def initDb(): Unit = {
+    val createZTableSql =
+      s"""CREATE TABLE "$tableName" (
+         |    Z1 varchar(255) NOT NULL,
+         |    Z2 int,
+         |    Z3 boolean,
+         |    Z4 varchar(255),
+         |    PRIMARY KEY (Z1)
+         |);""".stripMargin
+
+    val insertZDataSql =
+      s"""INSERT INTO "$tableName" (Z1,Z2,Z3,Z4) VALUES
+         |  ('r1', 1, true, 's1'),
+         |  ('r2', 2, false, 's2'),
+         |  ('r3', 3, NULL, 's3'),
+         |  ('r4', 45, true, 's4'),
+         |  ('r5', 45, true, 's5')
+         |;""".stripMargin
+
+    val addIndex1 =
+      s"""CREATE INDEX "${tableName.dropRight(5)}_idx1" ON "$tableName" (Z2);"""
+
+    val addIndex2 =
+      s"""CREATE INDEX "${tableName.dropRight(5)}_idx2" ON "$tableName" (Z3);"""
+
+    Await.result(for {
+      _ <- TestHelper.executeQuery(createZTableSql)
+      _ <- TestHelper.executeQuery(insertZDataSql)
+      _ <- TestHelper.executeQuery(addIndex1)
+      _ <- TestHelper.executeQuery(addIndex2)
+    } yield (), 5.seconds)
+  }
+
+  def cleanUpDb(): Unit = {
+    val dropZTableSql = s"""DROP TABLE "$tableName";"""
+    Await.result(for {
+      _ <- TestHelper.executeQuery(dropZTableSql)
+    } yield (), 15.seconds)
+  }
+}
